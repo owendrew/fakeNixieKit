b35de08b44c479655ae86a1a265b35cb0dc135e2
owendrew <62520182+owendrew@users.noreply.github.com>
2024-07-07 10:12:58 +0100

    Delete nixieClockConfig.h

--- nixieClockConfig.h
+++
@@ -1,7 +0,0 @@
-//Define your SSID and Password here
-#define SECRET_SSID "INSERT YOUR SSID"
-#define SECRET_PASS "INSERT YOUR SSID PASSWORD"
-
-//Define the Daylight Saving rule here
-#define DST_RULE "GMT0BST,M3.5.0/1,M10.5.0"
-//the rules are available here https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
